50b718b22e72ff710a0c415a2ffe1f493690bee9
Eric Flores <eflores13f@gmail.com>
2026-03-13 23:34:27 -0500

    Rename FileResizer project to PixelPress

---
+++ PixelPress/PixelPress/PixelPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelpressApp.swift
+//  FileResizer is being renamed to PixelPress
+//
+//  Created by eflo on 3/13/26.
+
+import SwiftUI
+
+@main
+struct PixelPressApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
